d0a98f5844e57bf1a82c044cdd44c3b3b2910821
teoma145 <matteodark145@yahoo.it>
2023-10-17 16:09:30 +0200

    for-es1

--- js/script.js
+++ js/script.js
@@ -4,6 +4,12 @@ let btn = document.getElementById('button');
 
 btn.addEventListener('click', function(){
     let palindromael = document.getElementById('palindromael').value;
-    console.log(palindromael)
+    
+    let checkpalindromo='';
+    for(let i = palindromael.length-1 ; i >= 0; i--){
+    checkpalindromo += palindromael[i];
+    console.log(checkpalindromo)
+}
+    
  
 })
