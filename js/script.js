90cbc6b1b69491114b26eafc28d5d8b38e7e2043
teoma145 <matteodark145@yahoo.it>
2023-10-17 15:46:49 +0200

    btnevent

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let btn = document.getElementById('button');
+
+
+
+btn.addEventListener('click', function(){
+    let palindromael = document.getElementById('palindromael').value;
+    console.log(palindromael)
+ 
+})
